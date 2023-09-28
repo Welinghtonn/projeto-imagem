b65a4cd92ef7f4b4d600ecc01010ba25648328af
Welinghtonn <126928676+Welinghtonn@users.noreply.github.com>
2023-09-28 18:07:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var array = []
+
+function Enviar() {
+  var input = document.getElementById("url")
+
+  if (input === "") {
+    return
+  } else {
+    array.push(input)
+  }
+
+  oii()
+}
+
+function oii() {
+  array.forEach((item) = )
+}
